580cedb3a09f31024c422851559603608d560b3d
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-16 16:46:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 require 'pry'
 def reverse_each_word(words)
     array = words.split
+    new = []
     array.each do |word|
-        new = word.reverse!
+        new << word.reverse!
         new
     end
 end
